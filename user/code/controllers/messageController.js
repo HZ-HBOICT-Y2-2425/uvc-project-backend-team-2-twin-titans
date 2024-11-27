443c836fc5bd738742282c79ca9420f576358a57
TijnLuckerhoff <luck0002@hz.nl>
2024-11-27 10:48:59 +0100

    Changed the createMessage function to check if the given user is in the chat

--- user/code/controllers/messageController.js
+++ user/code/controllers/messageController.js
@@ -19,17 +19,22 @@ export async function createMessage(req, res) {
   if (chat) {
     let berichten = chat.berichten;
     let gebruikerId = Number(req.query.userId);
-    // Check hoeveel berichten de chat met de opgegeven ID heeft, vervolgens pakt hij de ID van het laatste bericht en wordt er 1 bij toe gevoegt
-    let id = berichten[berichten.length - 1].id + 1;
-    let tekst = req.query.text;
-    let tijdstip = new Date().toLocaleString();
-    if (id && gebruikerId && tekst && tijdstip) {
-      let message = {id: id, gebruiker: gebruikerId, tekst: tekst, tijdstip: tijdstip};
-      berichten.push(message);
-      await db.write();
-      res.status(200).send(`Dit bericht is toegevoegt: ${JSON.stringify(message)} aan chat ${chatId}`);
+    let chatGebruikers = chat.gebruikers; 
+    if (chatGebruikers.lastIndexOf(gebruikerId) >= 0) {
+      // Check hoeveel berichten de chat met de opgegeven ID heeft, vervolgens pakt hij de ID van het laatste bericht en wordt er 1 bij toe gevoegt
+      let id = berichten[berichten.length - 1].id + 1;
+      let tekst = req.query.text;
+      let tijdstip = new Date().toLocaleString();
+      if (id && gebruikerId && tekst && tijdstip) {
+        let message = {id: id, gebruiker: gebruikerId, tekst: tekst, tijdstip: tijdstip};
+        berichten.push(message);
+        await db.write();
+        res.status(200).send(`Dit bericht is toegevoegt: ${JSON.stringify(message)} aan chat ${chatId}`);
+      } else {
+        res.status(404).send('Bericht mist een parameter');
+      }
     } else {
-      res.status(404).send('Bericht mist een parameter');
+      res.status(404).send('Opgegeven gebruiker niet gevonden in de chat');
     }
   } else {
     res.status(404).send('Chat niet gevonden');
