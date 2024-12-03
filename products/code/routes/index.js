6056d1d3fc5e5ae88d5dc8fb77f2fa16d62a4f16
Alex <lamp0020@hz.nl>
2024-12-03 15:10:11 +0100

    Updated db.json to add more products, updated getProducts controller function and updated index.js for routing

--- products/code/routes/index.js
+++ products/code/routes/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import * as productsController from '../controllers/productsController.js';
 const router = express.Router();
+router.use(cors());
 
 router.get('/', cors(), productsController.getProducts);
 
 router.post('/create', cors(), productsController.createProduct);
 
 router.get('/user/:userid', cors(), productsController.getProductsByUserID);
+
+router.get('/producten', productsController.getProducts);
 
 router.get('/product/:productid', cors(), productsController.getProduct);
 
