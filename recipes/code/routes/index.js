0cf1256aeeffe6b3f3b12638598255e21a713727
renzo <renzo.debeer06@gmail.com>
2024-12-03 09:58:07 +0100

    even om te switchen naar branch

--- recipes/code/routes/index.js
+++ recipes/code/routes/index.js
@@ -1,19 +1,9 @@
 import express from 'express';
-import { responseExample, updateExample, responseByIdExample } from '../controllers/exampleController.js';
 import { checkName } from '../middleware/exampleMiddleware.js';
 import { getAllRecipes, getRecipeById, test } from '../controllers/recipesController.js';
 const router = express.Router();
 
-// routes
-router.get('/test', checkName, test);
-router.get('/', (req, res, next) => {
-  res.json('hi');
-});
-router.get('/example', checkName, responseExample);
-router.post('/example', checkName, updateExample);
-router.get('/example/:id', checkName, responseByIdExample);
-
 // recipes routes
-router.get('/recipes', checkName, getAllRecipes);
-router.get('/recipes/:id', checkName, getRecipeById);
+router.get('/', cors(), checkName, getAllRecipes);
+router.get('/:id', cors(), checkName, getRecipeById);
 export default router;
