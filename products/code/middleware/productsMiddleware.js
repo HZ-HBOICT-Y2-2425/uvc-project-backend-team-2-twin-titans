60e0d706c833828cb7cf13d8178544be5a58cf28
Raweewee <t.kostert@outlook.com>
2024-12-17 12:21:55 +0100

    added image upload for products

--- products/code/middleware/productsMiddleware.js
+++ products/code/middleware/productsMiddleware.js
@@ -1,4 +1,34 @@
-export async function checkBody(req, res, next){
-    console.log(req.query);
-    next();
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import multer from 'multer';
+
+// Resolve __dirname in ES Module syntax
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Use __dirname to define the upload directory
+const uploadDir = path.join(__dirname, '../images');
+
+// Ensure the upload directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
+
+const upload = multer({
+  dest: uploadDir,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB file limit
+  fileFilter: (req, file, cb) => {
+
+    const filetypes = /jpeg|jpg|png/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb(new Error('Only .png, .jpg, and .jpeg formats are allowed.'));
+  },
+});
+
+export const handleImageUpload = upload.single('image');
--- products/code/routes/index.js
+++ products/code/routes/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import * as productsController from '../controllers/productsController.js';
+import * as productsMiddleware from '../middleware/productsMiddleware.js';
+
 const router = express.Router();
 
 router.get('/', cors(), productsController.getProducts);
 
-router.post('/create', cors(), productsController.createProduct);
+router.post('/create', cors(), productsMiddleware.handleImageUpload, productsController.createProduct);
+
+router.get('/product/image/:imagename', cors(), productsController.getImage);
 
 router.get('/product/:productid', cors(), productsController.getProduct);
 
 router.get('/user/:userid', cors(), productsController.getProductsByUserID);
 
-router.put('/update/:productid/userid', cors(), productsController.updateProduct);
+router.put('/update/:productid/:userid', cors(), productsMiddleware.handleImageUpload, productsController.updateProduct);
 
 router.delete('/delete/:productid/:userid', cors(), productsController.deleteProduct);
 
